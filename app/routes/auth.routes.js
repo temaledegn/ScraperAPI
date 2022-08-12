89f32df142a1fc80bfec419fee4785c567dcbfda
temaled <temaledyisfa@gmail.com>
2022-08-12 17:48:40 +0300

    update

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
-
+const express = require("express");
 const cors = require("cors"); 
+
+const app = express();
+app.use(express.json());
 app.use(cors());
 
 module.exports = function(app) {
