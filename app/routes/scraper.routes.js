b6c370fc99580560378cd87218e0e550a92721bd
temaled <temaledyisfa@gmail.com>
2023-06-06 13:39:27 +0300

    update

--- app/routes/scraper.routes.js
+++ app/routes/scraper.routes.js
@@ -111,7 +111,7 @@ module.exports = function (app) {
 
   // app.get("/common/keyword/search/", [cors(), authJwt.verifyToken, authJwt.isAdmin], controller.globalKeywordSearch); 
   
-  app.post("/common/keyword/twitter-live-search/", [cors(), authJwt.verifyToken, authJwt.isAdmin], controller.twitterKeywordSearch);
+  app.post("/common/keyword/twitter-live-search/", [cors(), authJwt.verifyToken, authJwt.isAdmin], controller.twitterLiveSearch);
 
 
 }
